0fa6c495fa5d4600ff3c659b6b76432ab6a3b57f
unknown <falcaoshadowbr@gmail.com>
2020-12-31 10:38:47 -0300

    Fix #2

--- src/pages/components/Menu/style.js
+++ src/pages/components/Menu/style.js
@@ -1,14 +1,5 @@
 import styled from 'styled-components'
 
-export const MenuStyle = styled.div`
-
-    nav ul li a:hover{
-        color: gold;
-        text-decoration: underline;
-        transform: scale(0.9);
-    }
-`
-
 export const Button = styled.a`
     padding: 5px;
     :hover{
